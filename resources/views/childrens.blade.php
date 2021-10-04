085b4ecf6c8fcd346112c784cb2f9d9eaabddcdd
dedoa82 <67242566+dedoa82@users.noreply.github.com>
2021-10-04 08:40:01 -0700

    Children module

---
+++ resources/views/childrens.blade.php
@@ -0,0 +1,132 @@
+@extends('layouts.master')
+@section('css')
+<!-- Internal Data table css -->
+<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+@endsection
+@section('page-header')
+				<!-- breadcrumb -->
+				<div class="breadcrumb-header justify-content-between">
+					<div class="my-auto">
+						<div class="d-flex">
+							<h4 class="content-title mb-0 my-auto">Pages</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ Empty</span>
+						</div>
+					</div>
+					<div class="d-flex my-xl-auto right-content">
+						<div class="pr-1 mb-3 mb-xl-0">
+							<a class="modal-effect btn btn-success-gradient btn-with-icon btn-block" data-effect="effect-scale" data-toggle="modal" href="#modaldemo8"><i class="typcn typcn-edit"></i> Create</a>
+						</div>
+						<div class="pr-1 mb-3 mb-xl-0">
+							<button class="btn btn-danger-gradient btn-with-icon btn-block"><i class="las la-trash"></i> Delete All</button>
+						</div>
+					</div>
+				</div>
+				<!-- breadcrumb -->
+@endsection
+@section('content')
+				<!-- row -->
+				<div class="row">
+                    <div class="col-xl-12">
+						<!-- Modal effects -->
+						<div class="modal" id="modaldemo8">
+							<div class="modal-dialog modal-dialog-centered" role="document">
+								<div class="modal-content modal-content-demo">
+									<div class="modal-header">
+										<h6 class="modal-title">Modal Header</h6><button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
+									</div>
+									<div class="modal-body">
+										<h6>Modal Body</h6>
+										<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+									</div>
+									<div class="modal-footer">
+										<button class="btn ripple btn-primary" type="button">Save changes</button>
+										<button class="btn ripple btn-secondary" data-dismiss="modal" type="button">Close</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- End Modal effects-->
+						<div class="card">
+							<div class="card-header pb-0">
+								<div class="d-flex justify-content-between">
+									<h4 class="card-title mg-b-0">STRIPED ROWS</h4>
+									<i class="mdi mdi-dots-horizontal text-gray"></i>
+								</div>
+								<p class="tx-12 tx-gray-500 mb-2">Example of Valex Striped Rows.. <a href="">Learn more</a></p>
+							</div>
+							<div class="card-body">
+								<div class="table-responsive">
+									<table class="table text-md-nowrap" id="example1">
+										<thead>
+											<tr>
+												<th>ID</th>
+												<th>child name</th>
+												<th>avatar</th>
+												<th>parent</th>
+												<th>phone</th>
+												<th>date</th>
+												<th>notes</th>
+												<th>Actions</th>
+											</tr>
+										</thead>
+										<tbody>
+											@foreach (DB::select('SELECT * FROM childrens') as $item)
+												<tr>
+													<th scope="row">{{$item->id}}</th>
+													<td>{{$item->child_name}}</td>
+													<td>
+														<img alt="Responsive image" class="img-thumbnail wd-55p wd-sm-55" src="http://127.0.0.1:8000/assets/img/photos/1.jpg">
+													</td>
+													<td>{{$item->parent}}</td>
+													<td>{{$item->phone}}</td>
+													<td>{{$item->date}}</td>
+													<td>{{$item->notes}}</td>
+													<td>
+														<a href="#" class="btn btn-md btn-info-gradient mg-t-2">
+															<i class="las la-pen"></i>
+														</a>
+														<a href="#" class="btn btn-md btn-danger-gradient mg-t-2">
+															<i class="las la-trash"></i>
+														</a>
+													</td>
+												</tr>
+											@endforeach
+										</tbody>
+									</table>
+								</div><!-- bd -->
+							</div><!-- bd -->
+						</div><!-- bd -->
+					</div>
+				</div>
+				<!-- row closed -->
+			</div>
+			<!-- Container closed -->
+		</div>
+		<!-- main-content closed -->
+@endsection
+@section('js')
+<!-- Internal Data tables -->
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+<!--Internal  Datatable js -->
+<script src="{{URL::asset('assets/js/table-data.js')}}"></script>
+<!-- Internal Modal js-->
+<script src="{{URL::asset('assets/js/modal.js')}}"></script>
+@endsection
