c5ba38bcc5af27e13a7fa7b5a79f3476999d4f4a
unknown <dedoa82.as@gmail.com>
2021-10-16 22:56:21 +0200

    Edit : pages header

--- resources/views/empty.blade.php
+++ resources/views/empty.blade.php
@@ -6,7 +6,7 @@
 				<div class="breadcrumb-header justify-content-between">
 					<div class="my-auto">
 						<div class="d-flex">
-							<h4 class="content-title mb-0 my-auto">Pages</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ Empty</span>
+							<h4 class="content-title mb-0 my-auto">Pages</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ فارغ</span>
 						</div>
 					</div>
 					<div class="d-flex my-xl-auto right-content">
