16f6a0a17d259fdf756d7db9a5c343120a853a42
unknown <dedoa82.as@gmail.com>
2021-10-26 01:19:56 +0200

    Create : Authentication module

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,59 @@
-@extends('layouts.app')
-
+@extends('layouts.master2')
+@section('css')
+<link href="{{URL::asset('css/unauth.css')}}" rel="stylesheet">
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+		<!-- Main-error-wrapper -->
+		<div class="d-flex justify-content-center h-100">
+			<div class="user_card">
+				<div class="d-flex justify-content-center">
+					<div class="brand_logo_container">
+						<img src="HarLogin.jpg" class="brand_logo" alt="Logo">
+					</div>
+				</div>
+				<div class="d-flex justify-content-center form_container">
+					<form method="POST" action="{{ route('login') }}" class="login-form">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+						{{--  --}}
+						<label for="email" class="col-form-label">البريد الالكترونى</label>
+						<div class="input-group mb-3">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-user"></i></span>
+							</div>
+							<input type="text" name="email" class="form-control input_user form-control @error('email') is-invalid @enderror" value="" >
+							<span class="invalid-feedback">
+								<strong>يوجد خطاء فى البريد الالكترونى او كلمة المرور</strong>
+							</span>
+						</div>
+						
+						{{--  --}}
+						<label for="">كلمة المرور</label>
+						<div class="input-group mb-2">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-key"></i></span>
+							</div>
+							<input type="password" name="password" class="form-control input_pass form-control @error('password') is-invalid @enderror" value="">
+						</div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+						{{--  --}}
+						<div class="form-group">
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customControlInline" name="remember"  {{ old('remember') ? 'checked' : '' }}>
+								<label class="custom-control-label" for="customControlInline">تذكرنى</label>
+							</div>
+						</div>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+						{{--  --}}
+						<div class="d-flex justify-content-center mt-3 login_container">
+				 			<button type="submit" name="button" class="btn login_btn">تسجيل دخول</button>
+				   		</div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		<!-- /Main-error-wrapper -->
+@endsection
+@section('js')
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,53 +16,58 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-
+    
 Auth::routes();
+// Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->group(function () {
 
-Route::resource('employees', 'EmployeeController');
+    Route::get('/', function () {
+        return view('index');
+    });
+    
+    Route::get('/home', 'HomeController@index')->name('home');
+    
+    Route::resource('employees', 'EmployeeController');
+    
+    Route::post('attendance_lists', function (Request $request) {
+    
+        DB::table('attendance_lists')->insert([
+            'employee_id'   => $request->employee_id,
+            'is_attende'    => $request->is_attende,
+            'delay_min'     => $request->delay_min,
+        ]);
+    
+        return response()->json(['status' => true]);
+    });
+    
+    Route::post('receivedSalary', function (Request $request) {
+    
+        // Make a export 
+        DB::table('financial_operations')->insert([
+            'amount'    => $request->finalsalary,
+            'client'    => $request->name,
+            'reason'    => "استلام راتب لشهر $request->month",
+            'receipt'   => $request->receipt,
+            'status'    => 0,
+        ]);
+    
+        // Make salaries received 
+        DB::table('salaries_received')->insert([
+            'employee_id' => $request->employee_id,
+            'is_received' => 1,
+            'month'       => $request->month
+        ]);
+    
+        return response()->json(['status' => true]);
+    });
+    
+    Route::get('/{page}', function ($page) {
+    
+        if(view()->exists($page)) return view($page);
+    
+        return view('404');
+    });
 
-Route::post('attendance_lists', function (Request $request) {
-
-    DB::table('attendance_lists')->insert([
-        'employee_id'   => $request->employee_id,
-        'is_attende'    => $request->is_attende,
-        'delay_min'     => $request->delay_min,
-    ]);
-
-    return response()->json(['status' => true]);
 });
-
-Route::post('receivedSalary', function (Request $request) {
-
-    // Make a export 
-    DB::table('financial_operations')->insert([
-        'amount'    => $request->finalsalary,
-        'client'    => $request->name,
-        'reason'    => "استلام راتب لشهر $request->month",
-        'receipt'   => $request->receipt,
-        'status'    => 0,
-    ]);
-
-    // Make salaries received 
-    DB::table('salaries_received')->insert([
-        'employee_id' => $request->employee_id,
-        'is_received' => 1,
-        'month'       => $request->month
-    ]);
-
-    return response()->json(['status' => true]);
-});
-
-Route::get('/{page}', function ($page) {
-
-    if(view()->exists($page)) return view($page);
-
-    return view('404');
-});
-
 
