744e1d0f74031b49b14662806c32a0cf27e8fba5
unknown <dedoa82.as@gmail.com>
2021-10-25 18:08:55 +0200

    Edit : headers meta tages

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,10 @@
 		<meta name="Author" content="Ahmed Med">
 		<!-- CSRF Token -->
 		<meta name="csrf-token" content="{{ csrf_token() }}">
-		
+		<!-- Title -->
+		<title> Harvest Financial Mangment System </title>
+		<!-- Favicon -->
+		<link rel="icon" href="{{URL::asset('assets/img/brand/favicon.png')}}"/>
 		@include('layouts.head')
 		@livewireStyles
 		
