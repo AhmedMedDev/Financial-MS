570eba739b89d364a9dc86dd7217df307438ae13
unknown <dedoa82.as@gmail.com>
2021-10-26 02:41:38 +0200

    Improvment : Landing Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,17 +10,15 @@
 		<div class="left-content">
 			<div>
 				<h2 class="main-content-title tx-24 mg-b-1 mg-b-lg-1">الصفحة الرئيسية</h2>
-				<p class="mg-b-0">للاطلاع على اخر التطورات</p>
+				<p class="mg-b-0">احصائيات سريعةعن اخر التطورات</p>
 			</div>
 		</div>
 		<div class="main-dashboard-header-right">
 			<div>
-				<label class="tx-13">صافى ربح هذا العام</label>
-				<h5>00,00</h5>
+				
 			</div>
 			<div>
-				<label class="tx-13">خسارة</label>
-				<h5>00,00</h5>
+				
 			</div>
 		</div>
 @endsection
